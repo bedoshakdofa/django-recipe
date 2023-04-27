50230f200668f2009678d55de3078843a3ee412a
bedoshakdofa <abdullahshakdofa@gmail.com>
2023-04-27 18:38:41 +0200

    django-recipe

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import recipe
 
 class registertion(UserCreationForm):
     class Meta:
         model=User
         fields=['username','email','password1','password2']
+
+class recipeForm(forms.Form):
+    class Meta:
+        model=recipe
+        fields=['__all__']
--- recipe/models.py
+++ recipe/models.py
@@ -1,11 +1,26 @@
 from django.db import models
-from time import date
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 class recipe(models.Model):
+    x=[
+        ('dessert','dessert'),
+        ('chicken','Chicken'),
+        ('meat','Meat'),
+        ('drinks','drinks'),
+    ]
+
+    option=[
+        ('puplished','puplished'),
+        ('draft','draft')
+    ]
     recipe_name=models.CharField(max_length=20)
-    recipe_ingred=models.TextField(max_length=20,null=True)
-    img=models.ImageField()
+    recipe_ingred=models.TextField(null=True)
+    img=models.ImageField(upload_to='photo/%y/%m/%d',default=1)
+    recipe_step=models.TextField(max_length=2000,default=1)
+    recipe_category=models.CharField(max_length=30,null=False,choices=x,default=1)
+    status=models.CharField(max_length=20,choices=option,default=1)
+    puplished=models.DateTimeField(default=timezone.now )
+    auther=models.ForeignKey(User,on_delete=models.CASCADE,default=1)
     def __str__(self):
         return self.id
-    
-    
+
